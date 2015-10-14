bf1a9dda42186b5b6b92c2463750df866a28b368
Zac Clemans <zaclem01@gmail.com>
2015-10-13 21:26:27 -0400

    add map modifications

---
+++ enumerable_modifications_map_proc.rb
@@ -0,0 +1,129 @@
+module Enumerable
+	def my_each
+		if block_given?
+			n = 0
+			while n < self.length
+				yield(self[n])
+				n += 1
+			end
+		else
+			self
+		end
+	end
+
+	def my_each_with_index
+		if block_given?
+			n = 0
+			while n < self.length
+				yield(self[n], n)
+				n += 1
+			end
+		else
+			self
+		end
+	end
+
+	def my_select
+		if block_given?
+			output = []
+			self.my_each { |item| output.push(item) if yield(item) }
+			return output
+		else
+			self
+		end
+	end
+
+	def my_all?
+		output = true
+		if block_given?
+			self.my_each do |item|
+				if !yield(item)
+					output = false
+					break
+				end
+			end
+		else
+			self.my_each do |item|
+				if !item
+					output = false
+					break
+				end
+			end
+		end
+		output
+	end
+
+	def my_any?
+		output = false
+		if block_given?
+			self.my_each do |item|
+				if yield(item)
+					output = true
+					break
+				end
+			end
+		else
+			self.my_each do |item|
+				if item
+					output = true
+					break
+				end
+			end
+		end
+		output
+	end
+
+	def my_none?
+		output = true
+		if block_given?
+			self.my_each do |item|
+				if yield(item)
+					output = false
+					break
+				end
+			end
+		else
+			self.my_each do |item|
+				if item
+					output = false
+					break
+				end
+			end
+		end
+		output
+	end
+
+	def my_count(match=false)
+		output = 0
+		if block_given?
+			self.my_each { |item| output += 1 if yield(item) }
+		elsif !match
+			self.my_each { |item| output += 1 }
+		else
+			self.my_each { |item| output += 1 if item == match }
+		end
+		output
+	end
+
+	def my_map(proc)
+		output = []
+		self.my_each { |item| output.push(proc.call(item)) }
+		output
+	end
+
+	def my_inject(initial=false)
+		if !initial
+			memo = 0
+			self.my_each { |item| memo = yield(memo, item) }
+		else
+			memo = initial
+			self.my_each { |item| memo = yield(memo, item) }
+		end
+		memo
+	end
+
+end
+
+square = Proc.new { |x| x**2 }
+
+p [1, 2, 3, 4].my_map(square)
